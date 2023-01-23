8337ab666b314d42abcbda9c9a5bc233debc8471
unknown <svenandreasclausz@gmail.com>
2023-01-23 17:53:14 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Button } from '@mui/material';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo! Otra vez
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant='contained'>Start</Button>
       </header>
     </div>
   );
