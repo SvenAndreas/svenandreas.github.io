2428772b361a14962e9a28f61009dafabc29b5b4
Sven Andreas Clausz <svendev@Svens-MacBook-Pro.local>
2024-08-15 20:09:50 -0300

    Hero realizado, navbar avanzada con nuevo logo y nombre, mas agregado de fonts

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,33 +1,33 @@
-import React, { useContext } from 'react'
-import s from './Home.module.css'
-import { SlSocialLinkedin } from 'react-icons/sl'
-import { RiGithubLine } from 'react-icons/ri'
-import { AiOutlineSend } from 'react-icons/ai'
-import { CiDesktopMouse2 } from 'react-icons/ci'
-import { AiOutlineArrowDown } from 'react-icons/ai'
-import profile from '../../Assets/perfil.png'
+import React, { useContext } from "react";
+import s from "./Home.module.css";
+import { SlSocialLinkedin } from "react-icons/sl";
+import { RiGithubLine } from "react-icons/ri";
+import { AiOutlineSend } from "react-icons/ai";
+import { CiDesktopMouse2 } from "react-icons/ci";
+import { AiOutlineArrowDown } from "react-icons/ai";
+import profile from "../../Assets/perfil.png";
+import logo from '../../Assets/evoluciondigital.png'
 // import useScrollEffect from '../../CustomHooks/useScrollEffect'
-import { LanguageContext } from '../../Contexts/languageContext'
-
+import { LanguageContext } from "../../Contexts/languageContext";
 
 function Home() {
-  const {language} = useContext(LanguageContext)
+  const { language } = useContext(LanguageContext);
   // const imgRef=useScrollEffect()
   return (
-    <section className={`${s.home} section`} id='home'>
-      <div className={` ${s.home_container} container grid`}>
-        <div className={`${s.home_content} grid`}>
-            <div className={s.home_social}>
+    <section className={`${s.home}  ${s.home_overlay}`} id="home">
+      <div className={` ${s.home_container} `}>
+        <div className={`${s.home_content} `}>
+          {/* <div className={s.home_social}>
               <a href='https://www.linkedin.com/in/sven-andreas-clausz' target='_blank' rel='noreferrer' className={s.home_social_icon}>
                 <SlSocialLinkedin/>
               </a>
               <a href='https://github.com/SvenAndreas' target='_blank' rel='noreferrer' className={s.home_social_icon}>
                 <RiGithubLine/>
-              </a>
-            </div>
-            <div className={s.home_img}>
-              
-              {/* <svg viewBox="4 20 250 147" className={s.home_blob} style={{position:'relative'}}  >
+              </a>s
+            </div> */}
+          {/* <div className={s.home_img}> */}
+
+          {/* <svg viewBox="4 20 250 147" className={s.home_blob} style={{position:'relative'}}  >
                 <mask id="mask0" mask-type="alpha">
                   <rect x="0" y="0" width="150" height="187" fill="#fff"/>
                 </mask>
@@ -37,7 +37,7 @@ function Home() {
                 </g>
               </svg> */}
 
-              {/* <svg className={s.home_blob} style={{position:'relative'}} viewBox="0 0 200 187">
+          {/* <svg className={s.home_blob} style={{position:'relative'}} viewBox="0 0 200 187">
                 <mask id="mask0" mask-type="alpha">
                     <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
                     130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
@@ -55,7 +55,7 @@ function Home() {
               </g>
             </svg> */}
 
-            <svg className={s.home_blob} style={{position:'relative'}} viewBox="0 0 200 200  ">
+          {/* <svg className={s.home_blob} style={{position:'relative'}} viewBox="0 0 200 200  ">
               <circle cx="100" cy="100" r="100" fill="#fff"/>
               <mask id="mask0" mask-type="alpha">
                 <circle cx="100" cy="100" r="100" fill="#fff"/>
@@ -67,34 +67,91 @@ function Home() {
                         0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
                 <image className={s.home_blob_image} href={profile} x='30' y='0'/>
               </g>
-            </svg>
-                      
-            {/* <img src={profile} alt='prfo' className={s.home_img_content}/> */}
-            
-            </div>
+            </svg> */}
 
-            <div className={s.home_data}>
-                <h1 className={s.home_title}>{language === 'ES' ? 'Hola, soy Sven Andreas Clausz' :"Hi, I'm Sven Andreas Clausz"}</h1>
-                <h3 className={s.home_subtitle}>{language === 'ES' ? 'Desarrollador de software':"Software developer"}</h3>
-                {language === 'ES' ? <p className={s.home_description}>Construyo <span className={s.home_highligth}>sitios web y aplicaciones móviles (IOS & Android) y web</span>  intuitivas con diseños exclusivos, manteniéndome a la vanguardia de las últimas tendencias tecnológicas.<br></br> Ayudando a aumentar la eficiencia de tu negocio.</p> : <p className={s.home_description}>Developing <span className={s.home_highligth}>intuitive websites and applications for mobile (iOS & Android) and web</span> with exclusive designs, staying at the forefront of the latest technological trends.<br></br> Helping to <span className={s.home_highligth}>increase the efficiency of your business.</span></p>}
-                <a href='#contact' className='button_secondary'>
-                  {language === 'ES' ? 'Contáctame' : 'Contact Me'}
-                  <AiOutlineSend className='button_icon'/>
-                </a>
-            </div>
+          {/* <img src={profile} alt='prfo' className={s.home_img_content}/> */}
+
+          {/* </div> */}
+
+          <div className={s.home_data}>
+            {/* <div className={s.home_logo_container}>
+              <img
+                alt="Evolucion digital Logo, árbol digital con ramificaciones y raíces que simulan conexiones electrónicas."
+                width={1322}
+                height={1600}
+                className={s.home_logo_img}
+                src={logo}
+              />
+              <p className={s.home_logo}>
+                EVOLUCIÓN DIGITAL
+                <p className={s.home_logo_subtitle}>IMPULSÁ TU NEGOCIO</p>
+              </p>
+            </div> */}
+            <h1 className={s.home_title}>
+              {language === "ES"
+                ? "DESARROLLAMOS TECNOLOGÍA PARA"
+                : "We develop"}
+                <span className={s.home_title_secondpart}> <span className={s.home_title_highlight}>IMPULSAR TU NEGOCIO.</span> </span>
+            </h1>
+            {/* <h3 className={s.home_subtitle}>
+              {language === "ES"
+                ? "Desarrollador de software"
+                : "Software developer"}
+            </h3>
+            {language === "ES" ? (
+              <p className={s.home_description}>
+                Me especializo en desarrollar soluciones digitales
+                personalizadas para{" "}
+                <span className={s.home_highligth}>
+                  impulsar el crecimiento de tu negocio
+                </span>
+                . Desde sitios web y ecommerce hasta software y aplicaciones
+                móviles, nos enfocamos en{" "}
+                <span className={s.home_highligth}>
+                  crear experiencias digitales excepcionales que conectan con
+                  tus clientes.
+                </span>
+              </p>
+            ) : (
+              <p className={s.home_description}>
+                Developing{" "}
+                <span className={s.home_highligth}>
+                  intuitive websites and applications for mobile (iOS & Android)
+                  and web
+                </span>{" "}
+                with exclusive designs, staying at the forefront of the latest
+                technological trends.<br></br> Helping to{" "}
+                <span className={s.home_highligth}>
+                  increase the efficiency of your business.
+                </span>
+              </p>
+            )} */}
+            {/* <a href="#contact" className="button_secondary">
+              {language === "ES" ? "Contáctame" : "Contact Me"}
+              <AiOutlineSend className="button_icon" />
+            </a> */}
+            <a href="#contact" className={s.home_button}>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              DESPEGAR
+            </a>
+          </div>
         </div>
 
         <div className={s.home_scroll}>
-          <a href='#about' className={s.home_scroll_button}>
-            <CiDesktopMouse2 className={s.home_scroll_mouse}/>
+          <a href="#about" className={s.home_scroll_button}>
+            <CiDesktopMouse2 className={s.home_scroll_mouse} />
             <span className={s.home_scroll_name}>Scroll down</span>
-            <AiOutlineArrowDown className={s.home_scroll_arrow}/>
+            <AiOutlineArrowDown className={s.home_scroll_arrow} />
           </a>
         </div>
       </div>
+      {/* <div className={s.home_rigth_container}></div> */}
       {/* <img src={wave} alt='wave' className={s.home_wave}/> */}
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,91 +1,151 @@
-import React, {useContext, useState} from 'react'
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
-import IntegrationInstructionsOutlinedIcon from '@mui/icons-material/IntegrationInstructionsOutlined';
-import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
-import s from './NavBar.module.css'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import useDetectHover from '../../CustomHooks/useDetectHover';
-import { LanguageContext } from '../../Contexts/languageContext';
+import React, { useContext, useState } from "react";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import WorkOutlineOutlinedIcon from "@mui/icons-material/WorkOutlineOutlined";
+import IntegrationInstructionsOutlinedIcon from "@mui/icons-material/IntegrationInstructionsOutlined";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import s from "./NavBar.module.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import useDetectHover from "../../CustomHooks/useDetectHover";
+import { LanguageContext } from "../../Contexts/languageContext";
+import logo from '../../Assets/evoluciondigital.png'
 
 function NavBar() {
-  const [isActive,setIsActive] = useState(false)  
-  const handleToggleMenu = ()=>{
-   isActive ? setIsActive(!isActive) : setIsActive(!isActive)
-  }
-  const {hovered} = useDetectHover('dropdown',0)
-  const {language,setLanguage} = useContext(LanguageContext)
+  const [isActive, setIsActive] = useState(false);
+  const handleToggleMenu = () => {
+    isActive ? setIsActive(!isActive) : setIsActive(!isActive);
+  };
+  const { hovered } = useDetectHover("dropdown", 0);
+  const { language, setLanguage } = useContext(LanguageContext);
   return (
     <header className={s.nav}>
       <nav className={s.nav_container}>
-      <div className={s.nav_header}>
-        <a href='#about' className={s.nav_logo}>SVEN</a>
-        <div id='dropdown' className={s.nav_dropdown_container}>
-          <KeyboardArrowDownIcon className={`${s.nav_dropdown_icon} ${hovered ?  s.nav_dropdown_icon_hovered : ''}`}/>
-          <p>Idiomas</p>
-          <p className={s.nav_dropdown_lang}>{language}</p>
-          <ul className={`${s.nav_dropdown_menu} ${hovered ? s.nav_dropdown_menu_active :''}`}>
-            <li onClick={()=>setLanguage('ES')} >
-              <img className={s.nav_dropdown_img} src='https://upload.wikimedia.org/wikipedia/commons/8/89/Bandera_de_Espa%C3%B1a.svg' alt='Bandera de españa' title='
+        <div className={s.nav_header}>
+          <div className={s.nav_logo_container}>
+            <img alt="Evolucion digital Logo, árbol digital con ramificaciones y raíces que simulan conexiones electrónicas." width={1322} height={1600} className={s.nav_logo_img} src={logo}/>
+            <a href="#about" className={s.nav_logo}>
+              EVOLUCIÓN DIGITAL
+              <p className={s.nav_logo_subtitle}>IMPULSÁ TU NEGOCIO</p>
+            </a>
+          </div>
+          <div id="dropdown" className={s.nav_dropdown_container}>
+            <KeyboardArrowDownIcon
+              className={`${s.nav_dropdown_icon} ${
+                hovered ? s.nav_dropdown_icon_hovered : ""
+              }`}
+            />
+            <p className={s.nav_dropdown_lang_title}>Idiomas</p>
+            <p className={s.nav_dropdown_lang}>{language}</p>
+            <ul
+              className={`${s.nav_dropdown_menu} ${
+                hovered ? s.nav_dropdown_menu_active : ""
+              }`}
+            >
+              <li onClick={() => setLanguage("ES")}>
+                <img
+                  className={s.nav_dropdown_img}
+                  src="https://upload.wikimedia.org/wikipedia/commons/8/89/Bandera_de_Espa%C3%B1a.svg"
+                  alt="Bandera de españa"
+                  title="
 RelShot 263, Echando una mano, Public domain, via Wikimedia Commons
-' />
-              <p>Español</p>
-            </li>
-            <li onClick={()=>setLanguage('EN')}>
-              <img className={s.nav_dropdown_img} src='https://upload.wikimedia.org/wikipedia/commons/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg' alt='Bandera inglaterra' title='Hoshie, Public domain, via Wikimedia Commons'/>
-              <p>English</p>
-            </li>
-          </ul>
+"
+                />
+                <p>Español</p>
+              </li>
+              <li onClick={() => setLanguage("EN")}>
+                <img
+                  className={s.nav_dropdown_img}
+                  src="https://upload.wikimedia.org/wikipedia/commons/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg"
+                  alt="Bandera inglaterra"
+                  title="Hoshie, Public domain, via Wikimedia Commons"
+                />
+                <p>English</p>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
 
-        <div className={`${s.nav_menu} ${isActive ? s.show_menu : ''}`} id='nav_menu'>
+        <div
+          className={`${s.nav_menu} ${isActive ? s.show_menu : ""}`}
+          id="nav_menu"
+        >
           <ul className={s.nav_ul}>
             <li>
-              <a href='#home' className={s.nav_link} onClick={handleToggleMenu}>
-                <HomeOutlinedIcon className={s.nav_icon}/>{language === 'ES' ? 'Inicio' : 'Home'}
+              <a href="#home" className={s.nav_link} onClick={handleToggleMenu}>
+                <HomeOutlinedIcon className={s.nav_icon} />
+                {language === "ES" ? "Inicio" : "Home"}
               </a>
             </li>
             <li>
-              <a href='#about' className={s.nav_link} onClick={handleToggleMenu}>
-               <PersonOutlineOutlinedIcon className={s.nav_icon}/>{language === 'ES' ? 'Sobre mí' : 'About'}
+              <a
+                href="#about"
+                className={s.nav_link}
+                onClick={handleToggleMenu}
+              >
+                <PersonOutlineOutlinedIcon className={s.nav_icon} />
+                {language === "ES" ? "Sobre mí" : "About"}
               </a>
             </li>
             <li>
-              <a href='#skills' className={s.nav_link} onClick={handleToggleMenu}>
-               <ArticleOutlinedIcon className={s.nav_icon}/>{language === 'ES' ? 'Habilidades' : 'Skills'}
+              <a
+                href="#skills"
+                className={s.nav_link}
+                onClick={handleToggleMenu}
+              >
+                <ArticleOutlinedIcon className={s.nav_icon} />
+                {language === "ES" ? "Habilidades" : "Skills"}
               </a>
             </li>
             <li>
-              <a href='#service' className={s.nav_link} onClick={handleToggleMenu}>
-                <IntegrationInstructionsOutlinedIcon className={s.nav_icon}/>{language === 'ES' ? 'Servicios' : 'Services'}
+              <a
+                href="#service"
+                className={s.nav_link}
+                onClick={handleToggleMenu}
+              >
+                <IntegrationInstructionsOutlinedIcon className={s.nav_icon} />
+                {language === "ES" ? "Servicios" : "Services"}
               </a>
             </li>
             <li>
-              <a href='#portfolio' className={s.nav_link} onClick={handleToggleMenu}>
-               <WorkOutlineOutlinedIcon className={s.nav_icon}/>{language === 'ES' ? 'Portafolio' : 'Portfolio'}
+              <a
+                href="#portfolio"
+                className={s.nav_link}
+                onClick={handleToggleMenu}
+              >
+                <WorkOutlineOutlinedIcon className={s.nav_icon} />
+                {language === "ES" ? "Portafolio" : "Portfolio"}
               </a>
             </li>
             <li>
-              <a href='#contact' className={s.nav_link} onClick={handleToggleMenu}>
-                <SendOutlinedIcon className={s.nav_icon}/>{language === 'ES' ? 'Contacto' : 'Contact'}
+              <a
+                href="#contact"
+                className={s.nav_link}
+                onClick={handleToggleMenu}
+              >
+                <SendOutlinedIcon className={s.nav_icon} />
+                {language === "ES" ? "Contacto" : "Contact"}
               </a>
             </li>
           </ul>
-          <CloseOutlinedIcon onClick={handleToggleMenu} className={s.nav_close}/>
+          <CloseOutlinedIcon
+            onClick={handleToggleMenu}
+            className={s.nav_close}
+          />
         </div>
         <div>
-          <div >
-            <GridViewOutlinedIcon onClick={handleToggleMenu} className={s.nav_toggle}/>
+          <div>
+            <GridViewOutlinedIcon
+              onClick={handleToggleMenu}
+              className={s.nav_toggle}
+            />
           </div>
         </div>
       </nav>
     </header>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
